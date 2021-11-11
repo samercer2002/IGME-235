b212e3d121405e5584d485bec29374685b1e8001
samercer2002 <70397624+samercer2002@users.noreply.github.com>
2021-11-11 11:54:27 -0500

    updated project 2

--- Project2/JS/main.js
+++ Project2/JS/main.js
@@ -1,7 +1,17 @@
-window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
+window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked;
+start();};
 //API documentation https://docs.mhw-db.com
 let displayTerm = "";
-	
+let randomize
+
+
+function start()
+{
+    randomize = false;
+    let button = document.querySelector("input[type='checkbox']");
+    button.addEventListener("change",setRandom);
+}
+
 function searchButtonClicked(){
 
     const API_URL = "https://mhw-db.com/";
@@ -56,36 +66,74 @@ function dataLoaded(e)
         let results = obj;
         console.log("results.length = " + results.length);
         let bigString = "<h2>Results</h2>";
-        bigString += "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
-        bigString += "<div class='results'>";
         let limit = document.querySelector("#limit").value;
         let term = document.querySelector("#type").value;
+        bigString += "<p><i>Here are " + limit+ " results for '" + displayTerm + "'</i></p>";
+        bigString += "<div class='results'>";
         for(let i = 0; i < limit; i++)
         {
-            let result = results[i];
+            let result;
             let smallURL;
             let line;
+            if(randomize)
+            {
+                let randomNumber = getRandomIntInclusive(0,results.length-1);
+                result = results[randomNumber];
+                console.log(randomNumber);
+            }
+            else{
+                result = results[i];
+            }
+            
             switch(term)
             {
                 case "weapons":
-                    smallURL = result.assets.image;
+                    if(result.assets == null)
+                    {
+                        smallURL = "images/no-image-found.png";
+                    }
+                    else{
+                        smallURL = result.assets.image;
+                    }
+                    
                     if(!smallURL) smallURL = "images/no-image-found.png";
                     line = `<div class = 'result'><img src='${smallURL}' title = '${result.id}' />`;
+                    line += `<div class = 'data'>${result.name}<br>Type: ${result.type}<br>Damage: ${result.attack.display}<br> Damage Type: ${result.damageType}`
+                    if(result.elements.length != 0)
+                    {
+                        line += `<br>Elemental Damage: <ul>`;
+                        for(let i = 0; i < result.elements.length;i++)
+                        {
+                            line += `<li>Type: ${result.elements[i].type}</li><li>Damage:${result.elements[i].damage}</li>`;
+                        }
+                        line += `</div>`;
+                    }
+                    else{
+                        line += `</div>`;
+                    }
                     line += `</div>`;
                     break;
                 case "armor":
-                    smallURL = result.assets.imageMale;
+                    if(result.assets == null)
+                    {
+                        smallURL = "images/no-image-found.png";
+                    }
+                    else{
+                        smallURL = result.assets.imageMale;
+                    }
                     if(!smallURL) smallURL = "images/no-image-found.png";
                     line = `<div class = 'result'><img src='${smallURL}' title = '${result.id}' />`;
-                    line += `<div class = 'data'>Name: ${result.name}</div>`
+                    line += `<div class = 'data'><div class='top'>${result.name}<br>Defense:<ul><li>Base:${result.defense.base}</li><li>Max:${result.defense.max}</li></ul></div>
+                    <div class='bottomLeft'>Resistances:<ul><li>Fire:${result.resistances.fire}</li><li>Water:${result.resistances.water}</li><li>Ice:${result.resistances.ice}</li>
+                    <li>Thunder:${result.resistances.thunder}</li><li>Dragon:${result.resistances.dragon}</li></div></div>`
                     /*smallURL = result.assets.imageFemale;
                     if(!smallURL) smallURL = "images/no-image-found.png";
                     line += `<img src='${smallURL}' title = '${result.id}' />`;*/
                     line += `</div>`;
                     break;
-                case "charms":
+                /*case "charms":
                     line = "";
-                    break;
+                    break;*/
             }
 
             bigString += line;
@@ -99,3 +147,12 @@ function dataError(e)
     {
          console.log("An error occurred");   
     }
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+function setRandom()
+{
+    randomize = !randomize;
+}
